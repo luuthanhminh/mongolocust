8c455ac85049c12566f4d3befb5c40b9f25f3752
Minh <minhltuit@gmail.com>
2024-04-10 14:59:10 +0700

    update load test

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ import os
 
 DEFAULTS = {'DB_NAME': 'test_performence',
             'COLLECTION_NAME': 'test_performence',
-            'CLUSTER_URL': f'<your-cluster-url>',
+            'CLUSTER_URL': f'mongodb://adminUser:securePassword123@mongodb-0.mongodb-headless.mongodb-replicaset.svc.cluster.local:27017,mongodb-1.mongodb-headless.mongodb-replicaset.svc.cluster.local:27017/?replicaSet=rs0&readPreference=secondaryPreferred',
             'DOCS_PER_BATCH': 100,
             'INSERT_WEIGHT': 1,
             'FIND_WEIGHT': 3,
